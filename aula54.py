6c56489c1f402305294bfd192fa552b67a3062c0
Ubiratan Neto <Ubiratan.Neto@drivetel.local>
2026-05-04 14:59:38 +0100

    operacao com ternario - if, else

---
+++ aula54.py
@@ -0,0 +1,39 @@
+"""
+Faça uma lista de compras com listas, o usuário deve ter a possibilidade de inserir, apagar e listar valores da sua lista.
+Não permita que o programa quebre com erros de índicas inexistentes na lista.
+"""
+import os
+
+lista = []
+while True:
+    print('Insira uma opção')
+    opcao = input('[i]nserir [a]pagar [l]istar: ')
+
+    if opcao == 'i':
+        os.system('clear')
+        valor = input('Valor: ')
+        lista.append(valor)
+        print('i')
+    elif opcao == 'a':
+        indice_str = input('Escolha o índice para apagar: ')
+
+        try:
+            indice = int(indice_str)
+            del lista[indice]
+        except ValueError:
+            print('Por favor, digite número inteiro.')
+        except IndexError:
+            print('Índice não existe na lista')
+        except Exception:
+            print('Erro desconhecido')
+    elif opcao == 'l':
+        os.system('clear')
+        
+        if len(lista) == 0:
+            print('Nada para listar')
+
+        for i, valor in enumerate(lista):
+            print(i, valor)
+
+    else:
+        print('Por favor, escolha i, a ou l.')
