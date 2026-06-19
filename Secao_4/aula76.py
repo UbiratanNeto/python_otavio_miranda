cf91d8245af5e9db17a1c43f7810c73143c11703
Ubiratan Neto <Ubiratan.Neto@drivetel.local>
2026-06-19 10:07:15 +0100

    Aula 88

--- Secao_4/aula76.py
+++ Secao_4/aula76.py
@@ -1,27 +1,33 @@
-# Manipulando chaves e valores em dicionários
-pessoa = {}
+# Métodos úteis dos dicionários em Python
+# len - quantas chaves
+# keys - iterável com as chaves
+# values - iterável com os valores
+# items - iterável com chaves e valores
+# setdefault - adiciona valor se a chave não existe
+# copy - retorna uma cópia rasa (shallow copy)
+# get - obtém uma chave
+# pop - Apaga um item com a chave especificada (del)
+# popitem - Apaga o último item adicionado
+# update - Atualiza um dicionário com outro
+p1 = {
+    'nome': 'Luiz',
+    'sobrenome': 'Miranda',
+}
+# print(p1['nome'])
+# print(p1.get('nome', 'Não existe'))
 
-##
-##
-
-chave = 'nome'
-
-pessoa[chave] = 'Luiz Otávio'
-pessoa['sobrenome'] = 'Miranda'
-
-
-print(pessoa[chave])
-
-pessoa[chave] = 'Maria'
-
-del pessoa['sobrenome']
-print(pessoa)
-print(pessoa['nome'])
-
-# print(pessoa.get('sobrenome'))
-if pessoa.get('sobrenome') is None:
-    print('NÃO EXISTE')
-else:
-    print(pessoa['sobrenome'])
-
-# print('ISSO Não vai')
+# nome = p1.pop('nome')
+# print(nome)
+# print(p1)
+# ultima_chave = p1.popitem()
+# print(ultima_chave)
+# print(p1)
+# p1.update({
+#     'nome': 'novo valor',
+#     'idade': 30,
+# })
+# p1.update(nome='novo valor', idade=30)
+# tupla = (('nome', 'novo valor'), ('idade', 30))
+lista = [['nome', 'novo valor'], ['idade', 30]]
+p1.update(lista)
+print(p1)
